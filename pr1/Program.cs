272ad6497d2cedffa5708c1b19517d4694985131
Андрей Максимович Коробков <144508844+Korobkov124@users.noreply.github.com>
2025-04-08 19:42:08 +0700

    Добавьте файлы проекта.

---
+++ pr1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string Mark = "Toyota";
+        string Model = "Mark 2";
+        double Engine_capacity = 2.5;
+        bool is_crached = false;
+
+        Console.WriteLine($"Mark:{Mark}\nModel:{Model}\nEngine capacity:{Engine_capacity}\nIs it crashed? {is_crached}");
+    }
+}
